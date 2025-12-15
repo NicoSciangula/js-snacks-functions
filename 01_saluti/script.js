f2369d8837b943cc1fd77c86b65ef8c95af74de1
NicoSciangula <nico.sciangula99@gmail.com>
2025-12-15 18:32:31 +0100

    esercizio 01_saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function salutaUtente(nome){
+return `Ciao ${nome}`;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(salutaUtente(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
