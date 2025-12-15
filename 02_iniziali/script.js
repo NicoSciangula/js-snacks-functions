4f1a5c271619051e65fb14d2d696c81b921a5f79
NicoSciangula <nico.sciangula99@gmail.com>
2025-12-15 18:55:59 +0100

    esercizio 02_iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,13 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function primaLettera(lettera) {
+  for (let i = 0; i <= lettera.length; i++) {
+    return lettera[i].substring(0, 1);
+  }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(primaLettera(names));
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
