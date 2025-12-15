4729fcc7333498d6bcf8d61d0e19530297d324c4
NicoSciangula <nico.sciangula99@gmail.com>
2025-12-15 19:56:32 +0100

    esercizio 03_filtra_iniziali

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,18 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function soloUnaLettera(nomi) {
+  let soloLetteraA = [];
+  for (let i = 0; i < nomi.length; i++) {
+    if (nomi[i][0] === "A") {
+      soloLetteraA.push(nomi[i]);
+    }
+  }
+  return soloLetteraA;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(soloUnaLettera(names));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
