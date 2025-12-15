dd88079cfa5d8bc89064d474a7fdb5fe7aa41129
NicoSciangula <nico.sciangula99@gmail.com>
2025-12-15 20:21:49 +0100

    esercizio 04_conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,17 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
-
+const vocaliNellaParola = (vocali) => {
+  for (let i = 0; i < 10; i++) {
+    if (vocali[i] === "a" || vocali[i] === "i") {
+      console.log(`(${vocali[i]})`);
+    }
+  }
+  return vocali;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+vocaliNellaParola(word);
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
